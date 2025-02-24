6cf5cb7fec921de6d9910a4eb34f023852d2bf94
abdisetiakawan <bedikadiryt@gmail.com>
2025-02-24 07:19:13 +0700

    Adds news section routing
    Adds routes for a new news section, allowing access to news articles.  The existing pages routes are also slightly adjusted for clarity.

--- app/Config/Routes.php
+++ app/Config/Routes.php
@@ -2,9 +2,12 @@
 
 use CodeIgniter\Router\RouteCollection;
 use App\Controllers\Pages;
+use App\Controllers\News;
 
 /**
  * @var RouteCollection $routes
  */
 $routes->get('pages', [Pages::class, 'index']);
-$routes->get('(:segment)', [Pages::class, 'view']);
+$routes->get('pages/(:segment)', [Pages::class, 'view']);
+$routes->get('news', [News::class, 'index']);
+$routes->get('news/(:segment)', [News::class, 'show']);
